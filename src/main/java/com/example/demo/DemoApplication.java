8ebe55d9662db4d43768d0b9aef68e42f2bb2eac
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-05-03 14:42:58 +0800

    put api endpoints in controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello(){
-		return Arrays.asList(new Student(1L, "Jasmine", "test@test.com", LocalDate.of(1999, Month.MARCH, 22), 23));
 	}
 
 }
