b7b3ec6654a52fd477c25ba64b271c8283a2a132
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-05-03 16:58:59 +0800

    introduce dependency injection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired //auto inits StudentService for us, without this we need to init StudentService ourselves when we init the StudentController
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,15 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
 // Service layer contains the business logic
+
+@Service //Info on Service annotation: https://www.tutorialspoint.com/spring_boot/spring_boot_service_components.htm
 public class StudentService {
     public List<Student> getStudents(){
         return Arrays.asList(new Student(1L, "Jasmine", "test@test.com", LocalDate.of(1999, Month.MARCH, 22), 23));
