4c84c228f2ee5b4082d8263df80186f1610b9f20
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-05-07 14:07:29 +0800

    setup student table and connect to db

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,26 @@
 package com.example.demo.student;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity // this annotation specifies the class is an entity and is mapped to a database table
+@Table // this annotation specifies the name of the database table to be used for mapping
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1 //increment id by 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
